9cae18881eb0507a9328e66c66da9903cbb3d414
AngelAlmada <angelvirgo2000@gmail.com>
2025-07-25 09:58:50 -0700

    actualizacion de seguridad de cors2

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,7 @@ async function bootstrap() {
 
   // Habilita CORS para permitir peticiones desde otro origen
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        'http://localhost:5173',
-        'https://delivery-tacos.netlify.app',
-        'https://fastorder.bit-one.net',
-      ];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('No permitido por CORS'));
-      }
-    },
+    origin: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
