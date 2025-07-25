66db32594ad0c20d04be48a5566164e38bd977ba
AngelAlmada <angelvirgo2000@gmail.com>
2025-07-25 09:53:08 -0700

    nvo2

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,18 @@ async function bootstrap() {
 
   // Habilita CORS para permitir peticiones desde otro origen
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://delivery-tacos.netlify.app',
-      'https://fastorder.bit-one.net',
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://localhost:5173',
+        'https://delivery-tacos.netlify.app',
+        'https://fastorder.bit-one.net',
+      ];
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
